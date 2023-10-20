a28bc43859450abf4d337b495def81a1761a5fee
milandetruja <milandetruja2@gmail.com>
2023-10-20 23:22:50 +0530

    design floe change

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Header from "./common/Header/Header";
 import DashBoard from "./components/Screens/DahsBoard/DashBoard";
 import Signup from "./components/Screens/SignUp/Signup";
 import Login from "./components/Screens/Login/Login";
+import PageNotFound from "./common/NotFound/PageNotFound";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         {window.location.pathname !== "/" &&
-          window.location.pathname !== "/signup" && <Header />}
+          window.location.pathname !== "/signup" &&
+          window.location.pathname !== "*" && <Header />}
         <Routes>
           <Route path="/dashboard" element={<DashBoard />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<Login />} />
-          <Route path="*" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
